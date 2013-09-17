733e43a28c5377dbf4be0c13c37bb59bc2c8fe32
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-09-17 18:10:19 +0100

    Fix remaining copyrights.

--- data/tmap.ml
+++ data/tmap.ml
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-   Copyright %%COPYRIGHT%%. All rights reserved.
+   Copyright 2012. All rights reserved.
    Distributed under the BSD3 license, see license at the end of the file.
    %%NAME%% release %%VERSION%%
   ---------------------------------------------------------------------------*)
@@ -63,7 +63,7 @@ let dump pr_v ppf m =
   pp ppf "@,}"
 
 (*---------------------------------------------------------------------------
-   Copyright %%COPYRIGHT%%
+   Copyright 2012
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
