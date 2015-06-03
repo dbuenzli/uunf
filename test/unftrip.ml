aea3d6ed9bd2a44a470866011e13cdfca708563e
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-06-03 18:57:11 +0100

    Rewrite unftrip to use cmdliner.

--- test/unftrip.ml
+++ test/unftrip.ml
@@ -4,9 +4,11 @@
    %%NAME%% version %%VERSION%%
   ---------------------------------------------------------------------------*)
 
+let strf = Printf.sprintf
 let pp = Format.fprintf
-let pp_pos ppf d = pp ppf "%d.%d:(%d) "
+let pp_pos ppf d = pp ppf "%d.%d:(%d,%06X) "
   (Uutf.decoder_line d) (Uutf.decoder_col d) (Uutf.decoder_count d)
+  (Uutf.decoder_byte_count d)
 
 let pp_malformed ppf bs =
   let l = String.length bs in
@@ -17,7 +19,11 @@ let pp_malformed ppf bs =
 
 let exec = Filename.basename Sys.executable_name
 let log f = Format.eprintf ("%s: " ^^ f ^^ "@?") exec
-let log_malformed inf d bs = log "%s:%a: %a@." inf pp_pos d pp_malformed bs
+
+let input_malformed = ref false
+let log_malformed inf d bs =
+  input_malformed := true;
+  log "%s:%a: %a@." inf pp_pos d pp_malformed bs
 
 (* Output *)
 
@@ -31,8 +37,8 @@ let uchar_encoder enc =
   let e = Uutf.encoder enc (`Channel stdout) in
   fun v -> ignore (Uutf.encode e v)
 
-let out_fun dump oe =
-  if dump then uchar_dump Format.std_formatter else uchar_encoder oe
+let out_fun ascii oe =
+  if ascii then uchar_dump Format.std_formatter else uchar_encoder oe
 
 (* Trip *)
 
@@ -62,12 +68,12 @@ let normalize nf inf d first_dec out =                   (* normalize to nf. *)
   if Uutf.decoder_removed_bom d then add (`Uchar Uutf.u_bom);
   loop d first_dec
 
-let trip nf dump inf ienc =
+let trip nf inf enc ascii =
   try
     let ic = if inf = "-" then stdin else open_in inf in
-    let d = Uutf.decoder ?encoding:ienc (`Channel ic) in
+    let d = Uutf.decoder ?encoding:enc (`Channel ic) in
     let first_dec = Uutf.decode d in            (* guess encoding if needed. *)
-    let out = out_fun dump (Uutf.decoder_encoding d) in
+    let out = out_fun ascii (Uutf.decoder_encoding d) in
     begin match nf with
     | None -> id inf d first_dec out
     | Some nf -> normalize nf inf d first_dec out
@@ -76,96 +82,88 @@ let trip nf dump inf ienc =
     flush stdout;
   with Sys_error e -> log "%s@." e; exit 1
 
-(* Random *)
-
-let u_surrogate_count = 0xDFFF - 0xD800 + 1
-let uchar_count = (0x10FFFF + 1) - u_surrogate_count
-let r_uchar () =
-  let n = Random.int uchar_count in
-  if n > 0xD7FF then `Uchar (n + u_surrogate_count) else `Uchar n
-
-let rec r_id out rcount =
-  if rcount = 0 then out `End else (out (r_uchar ()); r_id out (rcount - 1))
-
-let r_normalize nf out rcount =
-  let n = Uunf.create nf in
-  let rec add v = match Uunf.add n v with
-  | `Uchar cp as u -> out u; add `Await
-  | `Await | `End -> ()
-  in
-  let rec loop rcount =
-    if rcount = 0 then (add `End; out `End) else
-    (add (r_uchar ()); loop (rcount - 1))
-  in
-  loop rcount
-
-let random nf dump rseed rcount =
-  log "Normalizing %d random characters with seed %d\n" rcount rseed;
-  Random.init rseed;
-  let out = out_fun dump `UTF_8 in
-  begin match nf with
-  | None -> r_id out rcount
-  | Some nf -> r_normalize nf out rcount
-  end;
-  flush stdout
-
 (* Version *)
 
-let version () = Format.printf
-  "%s %%VERSION%% @\nUnicode %s@." exec Uunf.unicode_version
+let unicode_version () = Format.printf "%s@." Uunf.unicode_version
 
-(* Main *)
+(* Cmd *)
 
-let main () =
-  let usage = Printf.sprintf
-    "Usage: %s [OPTION]... [INFILE]\n\
-    \ Normalizes Unicode text from stdin to stdout.\n\
-    \ If no input encoding is specified, it is guessed. If no normalization\n\
-    \ is specified NFC is used.\n\
-         Options:" exec
-  in
-  let cmd = ref `Trip in
-  let set_cmd v () = cmd := v in
-  let inf = ref "-" in
-  let err_inf () = raise (Arg.Bad "only one file can be specified") in
-  let set_inf f = if !inf <> "-" then err_inf () else inf := f in
-  let ienc = ref None in
-  let ienc_fun enc = match Uutf.encoding_of_string enc with
-  | Some enc -> ienc := Some enc
-  | None -> log "unsupported@ input@ encoding '%s',@ trying to guess.@." enc
-  in
-  let nf = ref (Some `NFC) in
-  let nf_fun v = match String.lowercase v with
-  | "nfc" -> nf := (Some `NFC) | "nfd" -> nf := (Some `NFD)
-  | "nfkc" -> nf := (Some `NFKC) | "nfkd" -> nf := (Some `NFKD)
-  | "none" -> nf := None
-  | v -> log "unsupported@ normalization@ form@ `%s',@ using NFC.@." v
-  in
-  let dump = ref false in
-  let rseed = ref (Random.self_init (); Random.int (1 lsl 30 - 1)) in
-  let rcount = ref 1_000_000 in
-  let nat s r v = if v > 0 then r := v else log "%s must be > 0, ignored\n" s in
-  let options = [
-    "-version", Arg.Unit (set_cmd `Version),
-    " Program and Unicode version";
-    "-dump", Arg.Set dump,
-    " Dump normalized scalar values in ASCII, one per line";
-    "-enc", Arg.String ienc_fun,
-    "<enc> Input encoding: UTF-8, UTF-16, UTF-16BE, UTF-16LE, ASCII, latin1";
-    "-nf", Arg.String nf_fun,
-    "<nf> Normal form: NFC, NFD, NFKC, NFKD or none (no normalization)";
-    "-r", Arg.Unit (set_cmd `Random), " Generate random characters.";
-    "-rseed", Arg.Int (nat "-rseed" rseed), "<int> Random seed";
-    "-rcount", Arg.Int (nat "-rcount" rcount),
-    "<int> Number of random characters to generate";]
-  in
-  Arg.parse (Arg.align options) set_inf usage;
-  match !cmd with
-  | `Trip -> trip !nf !dump !inf !ienc
-  | `Random -> random !nf !dump !rseed !rcount
-  | `Version -> version ()
+let do_cmd cmd nf inf enc ascii = match cmd with
+| `Unicode_version -> unicode_version ()
+| `Trip -> trip nf inf enc ascii
 
-let () = main ()
+(* Cmdline interface *)
+
+open Cmdliner
+
+let cmd =
+  let doc = "Output supported Unicode version." in
+  let unicode_version = `Unicode_version, Arg.info ["unicode-version"] ~doc in
+  Arg.(value & vflag `Trip [unicode_version])
+
+let nf_doc = "NORMALIZATION"
+let nf =
+  let docs = nf_doc in
+  let doc = "Normalization Form D (NFD), canonical decomposition." in
+  let nfd = Some `NFD, Arg.info ["nfd"] ~doc ~docs in
+  let doc = "Normalization Form C (NFC), canonical decomposition followed by \
+             canonical composition." in
+  let nfc = Some `NFC, Arg.info ["nfc"] ~doc ~docs in
+  let doc = "Normalization form KD (NFKD), compatibility decomposition." in
+  let nfkd = Some `NFKD, Arg.info ["nfkd"] ~doc ~docs in
+  let doc = "Normalization form KC (NFKC), compatibility decomposition \
+             followed by canonical composition." in
+  let nfkc = Some `NFKC, Arg.info ["nfkc"] ~doc ~docs in
+  Arg.(value & vflag None [nfd; nfc; nfkd; nfkc])
+
+let file =
+  let doc = "The input file. Reads from stdin if unspecified." in
+  Arg.(value & pos 0 string "-" & info [] ~doc ~docv:"FILE")
+
+let enc =
+  let enc = [ "UTF-8", `UTF_8; "UTF-16", `UTF_16; "UTF-16LE", `UTF_16LE;
+              "UTF-16BE", `UTF_16BE; "ASCII", `US_ASCII; "latin1", `ISO_8859_1 ]
+  in
+  let doc = strf "Input encoding, must %s. If unspecified the encoding is \
+                  guessed. The output encoding is the same as the input \
+                  encoding except for ASCII and latin1 where UTF-8 is output."
+            (Arg.doc_alts_enum enc)
+  in
+  Arg.(value & opt (some (enum enc)) None & info [ "e"; "encoding" ] ~doc)
+
+let ascii =
+  let doc = "Output the input text as newline (U+000A) separated Unicode
+             scalar values written in the US-ASCII charset."
+  in
+  Arg.(value & flag & info ["a"; "ascii"] ~doc)
+
+let cmd =
+  let doc = "normalize Unicode text" in
+  let man = [
+    `S "DESCRIPTION";
+    `P "$(tname) inputs Unicode text from stdin and rewrites it to stdout
+        according to a specified Unicode normalization form (see UAX 15).";
+    `P "If no normalization form is specified the character stream is left
+        intact.";
+    `P "Invalid byte sequences in the input are reported on stderr and
+        replaced by the Unicode replacement character (U+FFFD) in the output.";
+    `S nf_doc;
+    `S "OPTIONS";
+    `S "EXIT STATUS";
+    `P "$(tname) exits with one of the following values:";
+    `I ("0", "no error occured");
+    `I ("1", "a command line parsing error occured");
+    `I ("2", "the input text was malformed");
+    `S "BUGS";
+    `P "This program is distributed with the Uunf OCaml library.
+        See http://erratique.ch/software/uunf for contact information." ]
+  in
+  Term.(pure do_cmd $ cmd $ nf $ file $ enc $ ascii),
+  Term.info "unftrip" ~version:"%%VERSION%%" ~doc ~man
+
+let () = match Term.eval cmd with
+| `Error _ -> exit 1
+| _ -> if !input_malformed then exit 2 else exit 0
 
 (*---------------------------------------------------------------------------
    Copyright 2012 Daniel C. Bünzli
