a1aa99a08053b84411e829ea9cc6ab5c4cbead6d
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-11-02 21:18:56 +0100

    Make topkg lint happy.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,23 +5,22 @@ open Topkg
 
 let massage () =
   let ocaml = Conf.tool "ocaml" `Build_os in
-  OS.Cmd.run Cmd.(ocaml % "pkg/build_support.ml") >>= fun () ->
-  OS.Cmd.run Cmd.(ocaml % "pkg/get_tests.ml")
+  OS.Cmd.run Cmd.(ocaml % "pkg/build_support.ml")
 
 let distrib =
   (* FIXME OPAMv2, move this to an x-unicode-version field in the opam file. *)
   let watermarks = ("UNICODE_VERSION", `String "9.0.0") :: Pkg.watermarks in
-  let exclude_paths () =
-    Pkg.exclude_paths () >>| fun ps ->
-    ("test/NormalizationTest.txt" :: "support" :: ps)
-  in
+  let exclude_paths () = Pkg.exclude_paths () >>| fun ps -> ("support" :: ps) in
   Pkg.distrib ~watermarks ~massage ~exclude_paths ()
 
 let uutf = Conf.with_pkg "uutf"
 let cmdliner = Conf.with_pkg "cmdliner"
 
 let () =
-  Pkg.describe "uunf" ~distrib @@ fun c ->
+  let opams =
+    [Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["xmlm"; "uucd"])]
+  in
+  Pkg.describe "uunf" ~opams ~distrib @@ fun c ->
   let uutf = Conf.value c uutf in
   let cmdliner = Conf.value c cmdliner in
   Ok [ Pkg.mllib ~api:["Uunf"] "src/uunf.mllib";
