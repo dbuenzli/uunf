dbuenzli/uunf

commit f4c03630009518216b8c4d88e99fcb456cdc64aa

OCaml Uchar.t support.

src/uunf.ml
@@ -4,9 +4,9 @@
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
-type ret = [ `Uchar of int | `End | `Await ]
+type ret = [ `Uchar of Uchar.t | `End | `Await ]
 let pp_ret ppf v = match (v :> ret) with
-| `Uchar u -> Format.fprintf ppf "`Uchar %04X" u
+| `Uchar u -> Format.fprintf ppf "`Uchar %a" Uchar.dump u
 | `End -> Format.fprintf ppf "`End"
 | `Await -> Format.fprintf ppf "`Await"
 
@@ -27,12 +27,9 @@ let err_ended add =
 
 (* Characters *)
 
-type uchar = int
-let ux_eoi = max_int                 (* end of input, outside unicode range. *)
-let ux_soi = ux_eoi - 1            (* start of input, outside unicode range. *)
-let ux_none = ux_soi - 2                  (* no char, outside unicode range. *)
-let is_scalar_value i =
-  (0x0000 <= i && i <= 0xD7FF) || (0xE000 <= i && i <= 0x10FFFF)
+let ux_none = max_int                      (* no char, outside unicode range. *)
+let u_dumb =                                     (* placeholder, overwritten. *)
+  `Uchar (Uchar.of_int 0x0000)
 
 (* Normalization properties. *)
 
@@ -42,7 +39,8 @@ let nfc_boundary u = Uunf_tmapbool.get Uunf_data.nfc_boundary_map u
 let nfd_boundary u = Uunf_tmapbool.get Uunf_data.nfd_boundary_map u
 let nfkc_boundary u = Uunf_tmapbool.get Uunf_data.nfkc_boundary_map u
 let nfkd_boundary u = Uunf_tmapbool.get Uunf_data.nfkd_boundary_map u
-let ccc u = Uunf_tmapbyte.get Uunf_data.ccc_map u
+let _ccc u = Uunf_tmapbyte.get Uunf_data.ccc_map u
+let ccc u = Uchar.to_int u
 let decomp_prop u = Uunf_tmap.get Uunf_data.decomp_map u
 let compose_prop u = Uunf_tmap.get Uunf_data.compose_map u
 
@@ -60,6 +58,7 @@ module H = struct                            (* Hangul arithmetic constants. *)
 end
 
 let decomp u =
+  let u = Uchar.to_int u in
   if u < 0xAC00 || 0xD7A3 < u then decomp_prop u else
   begin                                        (* LV or LVT hangul composite *)
     let sindex = u - H.sbase in
@@ -75,7 +74,8 @@ let decomp u =
    indicate the non starter count increment. *)
 
 let d_compatibility i = i land (1 lsl 24) > 0
-let d_uchar i = i land 0x1FFFFF
+let _d_uchar i = i land 0x1FFFFF
+let d_uchar i = Uchar.unsafe_of_int (_d_uchar i)
 
 let _composite u1 u2 =
   if 0x1100 <= u1 && u1 <= 0x1112 then
@@ -103,8 +103,8 @@ let _composite u1 u2 =
       with Exit -> (a.(!i * 2 + 1))
 
 let composite u1 u2 =
-  let u = _composite u1 u2 in
-  if u = ux_none then None else Some u
+  let u = _composite (Uchar.to_int u1) (Uchar.to_int u2) in
+  if u = ux_none then None else Some (Uchar.unsafe_of_int u)
 
 (* Normalize *)
 
@@ -120,14 +120,16 @@ type t =
   { form : form;                                      (* normalization form. *)
     compat : bool;            (* true if compatibility decomposition needed. *)
     compose : bool;                           (* true if composition needed. *)
-    boundary : uchar -> bool;                               (* nfx_boundary. *)
+    boundary : int -> bool;                                 (* nfx_boundary. *)
     mutable state : state;                              (* normalizer state. *)
-    mutable uc : [`Uchar of uchar];         (* last cp with boundary = true. *)
+    mutable uc : [`Uchar of Uchar.t];       (* last cp with boundary = true. *)
     mutable acc : int array;                      (* code point accumulator. *)
     mutable first : int;                (* index of first code point in acc. *)
-    mutable last : int; }                (* index of last code point in acc. *)
+    mutable last : int;                  (* index of last code point in acc. *)
+    mutable is_end : bool;}                      (* [true] if `End was seen. *)
 
-let create_acc () = Array.make 35 ux_eoi
+
+let create_acc () = Array.make 35 ux_none
 let create form  =
   let boundary, compat, compose = match form with
   | `NFC -> nfc_boundary, false, true
@@ -136,29 +138,28 @@ let create form  =
   | `NFKD -> nfkd_boundary, true, false
   in
   { form = (form :> form); compat; compose; boundary; state = Start;
-    uc = `Uchar ux_soi; acc = create_acc (); first = 0; last = -1; }
+    uc = u_dumb; acc = create_acc (); first = 0; last = -1; is_end = false}
 
-let is_end n = let `Uchar u = n.uc in u = ux_eoi
-let get_u n = let `Uchar u = n.uc in u
+let get_u n = let `Uchar u = n.uc in Uchar.to_int u
 let acc_empty n = n.first > n.last
 let form n = n.form
 let copy n = { n with acc = Array.copy n.acc }
 let reset n =
-  n.state <- Start; n.uc <- `Uchar ux_soi; n.acc <- create_acc ();
-  n.first <- 0; n.last <- -1
+  n.state <- Start; n.uc <- u_dumb; n.acc <- create_acc ();
+  n.first <- 0; n.last <- -1; n.is_end <- false
 
 let grow_acc n =
   let len = Array.length n.acc in
-  let acc' = Array.make (2 * len) ux_eoi in
+  let acc' = Array.make (2 * len) ux_none in
   Array.blit n.acc 0 acc' 0 len; n.acc <- acc'
 
 let ordered_add n u =    (* canonical ordering algorithm via insertion sort. *)
   n.last <- n.last + 1; if n.last = Array.length n.acc then grow_acc n;
-  let c = ccc u in
+  let c = _ccc u in
   if c = 0 then n.acc.(n.last) <- u else
   begin
     let i = ref (n.last - 1) in
-    while (!i >= 0 && ccc (n.acc.(!i)) > c) do
+    while (!i >= 0 && _ccc (n.acc.(!i)) > c) do
       n.acc.(!i + 1) <- n.acc.(!i); decr i;                  (* shift right. *)
     done;
     n.acc.(!i + 1) <- u
@@ -180,7 +181,7 @@ let rec add n u =
     | d ->
         if d_compatibility d.(0) && not n.compat then ordered_add n u else
         begin
-          add n (d_uchar d.(0));
+          add n (_d_uchar d.(0));
           for i = 1 to Array.length d - 1 do add n d.(i) done
         end
     end
@@ -189,10 +190,10 @@ let compose n =                         (* canonical composition algorithm. *)
   let i = ref n.first in
   while (!i < n.last) do
     incr i;
-    let c = ccc n.acc.(!i) in
+    let c = _ccc n.acc.(!i) in
     try
       for j = !i - 1 downto n.first do
-        let c' = ccc n.acc.(j) in
+        let c' = _ccc n.acc.(j) in
         if c' = 0 then
           let u = _composite n.acc.(j) n.acc.(!i) in
           if u = ux_none then (* no composite => blocked *) raise Exit else
@@ -209,7 +210,7 @@ let compose n =                         (* canonical composition algorithm. *)
   done
 
 let flush_next n =
-  let ret = `Uchar n.acc.(n.first) in
+  let ret = `Uchar (Uchar.unsafe_of_int n.acc.(n.first)) in
   if n.first = n.last then (n.first <- 0; n.last <- -1) else
   (n.first <- n.first + 1);
   ret
@@ -217,6 +218,7 @@ let flush_next n =
 let flush_start n = if n.compose then compose n; flush_next n
 let add n = function
 | `Uchar u as uc ->
+    let u = Uchar.to_int u in
     begin match n.state with
     | Boundary ->
         if n.boundary u
@@ -237,15 +239,16 @@ let add n = function
     begin match n.state with
     | Flush ->
         if acc_empty n
-        then (n.state <- if is_end n then End else Boundary; `Await)
+        then (n.state <- if n.is_end then End else Boundary; `Await)
         else flush_next n
     | Start | Boundary | Acc -> `Await
     | End -> `End
     end
 | `End ->
+    n.is_end <- true;
     begin match n.state with
     | Boundary -> n.state <- End; (n.uc :> ret)
-    | Acc -> n.state <- Flush; n.uc <- `Uchar ux_eoi; flush_start n
+    | Acc -> n.state <- Flush; flush_start n
     | Start -> n.state <- End; `End
     | Flush -> err_exp_await `End
     | End -> err_ended `End

test/test.ml
@@ -28,12 +28,11 @@ let fail fmt =
 (* Conformance data decoding *)
 
 type conformance_test = int list array * string        (* columns + comment. *)
-module Cp = struct type t = int let compare : int -> int -> int = compare end
-module CpSet = Set.Make(Cp)             (* not a diet set, but will do here. *)
+module Uset = Set.Make(Uchar)          (* not a diet set, but will do here. *)
 
 let err_test_file () = invalid_arg "test data file invariant violated"
 
-let cp_of_string v =                           (* parses a code point value. *)
+let uchar_of_string v =                            (* parses a scalar value. *)
   let is_hex c = (0x30 <= c && c <= 0x39) || (0x41 <= c && c <= 0x46) in
   let cp = ref 0 in
   for k = 0 to (String.length v) - 1 do
@@ -41,9 +40,9 @@ let cp_of_string v =                           (* parses a code point value. *)
     if not (is_hex c) then failwith "" else
     cp := !cp * 16 + (if c <= 0x39 then c - 48 else c - 55)
   done;
-  !cp
+  Uchar.of_int !cp
 
-let cps_of_string v = List.map cp_of_string (split_string v ' ')
+let uchars_of_string v = List.map uchar_of_string (split_string v ' ')
 let decode_conformance_data ic =
   let rec loop tests collect_decomps decomps =
     match try Some (input_line ic) with End_of_file -> None with
@@ -57,13 +56,14 @@ let decode_conformance_data ic =
         | test :: comment :: _ ->
             begin match split_string test ';' with
             | c1 :: c2 :: c3 :: c4 :: c5 :: _ ->
-                let test = [| cps_of_string c1; cps_of_string c2;
-                              cps_of_string c3; cps_of_string c4;
-                              cps_of_string c5; |], comment
+                let test = [| uchars_of_string c1; uchars_of_string c2;
+                              uchars_of_string c3; uchars_of_string c4;
+                              uchars_of_string c5; |], comment
                 in
                 let decomps =
                   if not collect_decomps then decomps else
-                  match (fst test).(0) with [ cp ] -> CpSet.add cp decomps
+                  match (fst test).(0) with [ uchar ] ->
+                    Uset.add uchar decomps
                   | _ -> failwith ""
                 in
                 loop (test :: tests) collect_decomps decomps
@@ -73,7 +73,7 @@ let decode_conformance_data ic =
           log "Unable to parse line:\n`%s'\n" l;
           loop tests collect_decomps decomps
   in
-  loop [] false CpSet.empty
+  loop [] false Uset.empty
 
 (* Tests *)
 
@@ -141,19 +141,26 @@ let test_conformance_non_decomposables decomps =
     List.rev (add (add [] (`Uchar u)) `End)
   in
   let check u =
-    if CpSet.mem u decomps then () else
+    if Uset.mem u decomps then () else
     begin
       let ul = [u] in
       Uunf.reset nc; Uunf.reset nd; Uunf.reset nkc; Uunf.reset nkd;
-      if norm nc u <> ul then fail "NFC: U+%04X <> toNFC(U+%04X)" u u;
-      if norm nd u <> ul then fail "NFD: U+%04X <> toNFD(U+%04X)" u u;
-      if norm nkc u <> ul then fail "NFKC: U+%04X <> toNFKC(U+%04X)" u u;
-      if norm nkd u <> ul then fail "NFKD: U+%04X <> toNFKD(U+%04X)" u u;
+      if norm nc u <> ul then
+        fail "NFC: %a <> toNFC(%a)" Uchar.dump u Uchar.dump u;
+      if norm nd u <> ul then
+        fail "NFD: %a <> toNFD(%a)" Uchar.dump u Uchar.dump u;
+      if norm nkc u <> ul then
+        fail "NFKC: %a <> toNFKC(%a)" Uchar.dump u Uchar.dump u;
+      if norm nkd u <> ul then
+        fail "NFKD: %a <> toNFKD(%a)" Uchar.dump u Uchar.dump u;
     end
   in
   (* For each unicode scalar value *)
-  for u = 0x0000 to 0xD7FF do check u done;
-  for u = 0xE000 to 0x10FFFF do check u done
+  let rec loop u =
+    if Uchar.equal Uchar.max u then check u else
+    (check u; loop (Uchar.succ u))
+  in
+  loop Uchar.min
 
 let test_others () =
   let test src nf dst =
@@ -162,8 +169,9 @@ let test_others () =
     | `Uchar u -> add (u :: acc) `Await
     | `Await | `End -> acc
     in
-    let add_uchar acc u = add acc (`Uchar u) in
+    let add_uchar acc u = add acc (`Uchar (Uchar.of_int u)) in
     let nseq = List.rev (add (List.fold_left add_uchar [] src) `End) in
+    let dst = List.map Uchar.of_int dst in
     if nseq <> dst then fail ""
   in
   test [0x1E69] `NFD [0x0073; 0x0323; 0x0307];

src/uunf.mli
@@ -7,9 +7,9 @@
 (** Unicode text normalization.
 
     [Uunf] normalizes Unicode text.  It supports all Unicode
-    normalization forms. The module is independent from any IO mechanism or
-    Unicode text data structure and it can process text without a
-    complete in-memory representation of the data.
+    normalization forms. The module is independent from any IO
+    mechanism or Unicode text data structure and it can process text
+    without a complete in-memory representation of the data.
 
     The supported Unicode version is determined by the {!unicode_version}
     value.
@@ -32,19 +32,6 @@
     {e {{:http://www.unicode.org/charts/normalization/}Normalization charts}.
     }}} *)
 
-(** {1 Characters} *)
-
-type uchar = int
-(** The type for Unicode characters. A value of this type {b must}
-    be an Unicode
-    {{:http://www.unicode.org/glossary/#unicode_scalar_value} scalar value}
-    which is an integer value in the ranges [0x0000]...[0xD7FF]
-    and [0xE000]...[0x10FFFF]. *)
-
-val is_scalar_value : int -> bool
-(** [is_scalar_value n] is [true] iff [n] is an Unicode
-    {{:http://www.unicode.org/glossary/#Unicode_scalar_value}scalar value}. *)
-
 (** {1 Normalize} *)
 
 type form = [ `NFD | `NFC | `NFKD | `NFKC ]
@@ -65,7 +52,7 @@ type form = [ `NFD | `NFC | `NFKD | `NFKC ]
 type t
 (** The type for Unicode text normalizers. *)
 
-type ret = [ `Uchar of uchar | `End | `Await ]
+type ret = [ `Uchar of Uchar.t | `End | `Await ]
 (** The type for normalizer results. See {!add}. *)
 
 val create : [< form ] -> t
@@ -74,7 +61,7 @@ val create : [< form ] -> t
 val form : t -> form
 (** [form n] is the normalization form of [n]. *)
 
-val add : t -> [ `Uchar of uchar | `Await | `End ] -> ret
+val add : t -> [ `Uchar of Uchar.t | `Await | `End ] -> ret
 (** [add n v] is:
     {ul
     {- [`Uchar u] if [u] is the next character in the normalized
@@ -117,12 +104,12 @@ val pp_ret : Format.formatter -> ret -> unit
 val unicode_version : string
 (** [unicode_version] is the Unicode version supported by the module. *)
 
-val ccc : uchar -> int
+val ccc : Uchar.t -> int
 (** [ccc u] is [u]'s
     {{:http://www.unicode.org/glossary/#combining_class}canonical combining
     class} value. *)
 
-val decomp : uchar -> int array
+val decomp : Uchar.t -> int array
 (** [decomp u] is [u]'s
     {{:http://www.unicode.org/glossary/#decomposition_mapping}decomposition
     mapping}. If the empty array is returned, [u] decomposes to itself.
@@ -131,17 +118,17 @@ val decomp : uchar -> int array
     cannot be used as an {!uchar}. Use {!d_uchar} on the number to get the
     actual character and {!d_compatibility} to find out if this is
     a compatibility decomposition. All other characters of the array
-    are guaranteed to be of type {!uchar}.
+    are guaranteed to be convertible using {!Uchar.of_int}.
 
     {b Warning.} Do {b not} mutate the array. *)
 
-val d_uchar : int -> uchar
+val d_uchar : int -> Uchar.t
 (** See {!decomp}. *)
 
 val d_compatibility : int -> bool
 (** See {!decomp}. *)
 
-val composite : uchar -> uchar -> uchar option
+val composite : Uchar.t -> Uchar.t -> Uchar.t option
 (** [composite u1 u2] is the
     {{:http://www.unicode.org/glossary/#primary_composite}primary composite}
     canonically equivalent to  the sequence [<u1,u2>], if any. *)
@@ -188,7 +175,8 @@ let rec add acc v = match Uunf.add nfd v with
     For example to normalize the character [U+00E9] (é) with [nfd] to a list
     of characters we can write:
 {[
-let e_acute_nfd = List.rev (add (add [] (`Uchar 0x00E9)) `End)
+let e_acute = Uchar.of_int 0x00E9
+let e_acute_nfd = List.rev (add (add [] (`Uchar e_acute)) `End)
 ]}
     The next section has more examples.
 *)

test/test_string.ml
@@ -8,7 +8,7 @@ let test_others enc normalize =
   let b = Buffer.create 42 in
   let enc us =
     let rec loop = function
-    | u :: us -> enc b u; loop us
+    | u :: us -> enc b (Uchar.of_int u); loop us
     | [] -> Buffer.contents b
     in
     Buffer.reset b; loop us

test/unftrip.ml
@@ -28,7 +28,7 @@ let log_malformed inf d bs =
 (* Output *)
 
 let uchar_dump ppf = function
-| `End -> () | `Uchar u -> pp ppf "%a@\n" Uutf.pp_cp u
+| `End -> () | `Uchar u -> pp ppf "%a@\n" Uchar.dump u
 
 let uchar_encoder enc =
   let enc = match enc with `ISO_8859_1 | `US_ASCII -> `UTF_8

commit 9d040be276d07fccc700daa3fbaa192151f8b983

Fix regression in Uunf.ccc introduced by f4c0363 (closes #9).

src/uunf.ml
@@ -41,7 +41,7 @@ let nfd_boundary u = Uunf_tmapbool.get Uunf_data.nfd_boundary_map u
 let nfkc_boundary u = Uunf_tmapbool.get Uunf_data.nfkc_boundary_map u
 let nfkd_boundary u = Uunf_tmapbool.get Uunf_data.nfkd_boundary_map u
 let _ccc u = Uunf_tmapbyte.get Uunf_data.ccc_map u
-let ccc u = Uchar.to_int u
+let ccc u = _ccc (Uchar.to_int u)
 let decomp_prop u = Uunf_tmap.get Uunf_data.decomp_map u
 let compose_prop u = Uunf_tmap.get Uunf_data.compose_map u
 

test/test.ml
@@ -195,6 +195,11 @@ let test_others () =
   test [0x0041; 0x007A; 0x0335; 0x0327; 0x0324; 0x0301; 0x0041] `NFC
        [0x0041; 0x017A; 0x0335; 0x0327; 0x0324; 0x0041]
 
+let test_ccc () =
+  assert (Uunf.ccc (Uchar.of_int 0x0020) = 0);
+  assert (Uunf.ccc (Uchar.of_int 0x0301) = 230);
+  ()
+
 let test inf =
   try
     let ok () = log "[DONE]\n" in
@@ -207,6 +212,8 @@ let test inf =
     ok (); log "Testing conformance of non-decomposable characters... ";
     test_conformance_non_decomposables decomps;
     ok (); log "Unicode normalization conformance tests passed!\n";
+    log "Testing Uunf.ccc";
+    test_ccc ();
     log "Making other tests... ";
     test_others ();
     ok (); log "Success!\n"