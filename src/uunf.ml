dbuenzli/uunf

commit 9459c907e84eb9feaf3701ba48bf632c0eb801a0

Fix bug in canonical composition algorithm (closes #8).

src/uunf.ml
@@ -129,7 +129,6 @@ type t =
     mutable last : int;                  (* index of last code point in acc. *)
     mutable is_end : bool;}                      (* [true] if `End was seen. *)
 
-
 let create_acc () = Array.make 35 ux_none
 let create form  =
   let boundary, compat, compose = match form with
@@ -188,27 +187,27 @@ let rec add n u =
     end
 
 let compose n =                         (* canonical composition algorithm. *)
-  let i = ref n.first in
-  while (!i < n.last) do
-    incr i;
-    let c = _ccc n.acc.(!i) in
-    try
-      for j = !i - 1 downto n.first do
-        let c' = _ccc n.acc.(j) in
-        if c' = 0 then
-          let u = _composite n.acc.(j) n.acc.(!i) in
-          if u = ux_none then (* no composite => blocked *) raise Exit else
-          begin
-            n.acc.(j) <- u;
-            Array.blit n.acc (!i + 1) n.acc !i (n.last - !i);
-            decr i;
-            n.last <- n.last - 1
-          end
-        else
-        if c' >= c then (* blocked *) raise Exit
-      done
-    with Exit -> ()
-  done
+  let rec loop ~last_starter ~prev_ccc i =
+    if i > n.last then () else
+    match _composite n.acc.(last_starter) n.acc.(i) with
+    | u when u = ux_none ->
+        let ccc_i = _ccc n.acc.(i) in
+        let last_starter = if ccc_i = 0 then i else last_starter in
+        loop ~last_starter ~prev_ccc:ccc_i (i + 1)
+    | u ->
+        let ccc_i = _ccc n.acc.(i) in
+        match prev_ccc <> 0 && prev_ccc >= ccc_i with
+        | true -> loop ~last_starter ~prev_ccc:ccc_i (i + 1)
+        | false ->
+            n.acc.(last_starter) <- u;
+            Array.blit n.acc (i + 1) n.acc i (n.last - i);
+            n.last <- n.last - 1;
+            let prev_ccc = _ccc n.acc.(last_starter) in
+            loop ~last_starter ~prev_ccc (last_starter + 1)
+  in
+  let last_starter = n.first in
+  let prev_ccc = _ccc n.acc.(last_starter) in
+  loop ~last_starter ~prev_ccc (last_starter + 1)
 
 let flush_next n =
   let ret = `Uchar (Uchar.unsafe_of_int n.acc.(n.first)) in

test/test.ml
@@ -193,7 +193,11 @@ let test_others () =
   test [0x1E9B; 0x0323] `NFKD [0x0073; 0x0323; 0x0307];
   test [0x1E9B; 0x0323] `NFKC [0x1E69];
   test [0x0041; 0x007A; 0x0335; 0x0327; 0x0324; 0x0301; 0x0041] `NFC
-       [0x0041; 0x017A; 0x0335; 0x0327; 0x0324; 0x0041]
+       [0x0041; 0x017A; 0x0335; 0x0327; 0x0324; 0x0041];
+  (* found by crowbar *)
+  test [0x01C6; 0x032D] `NFKC [0x0064; 0x017E; 0x032D];
+  test [0xFF80; 0x1FD3; 0xFF9E; 0x1FD3;] `NFKC [0x30BF; 0x0390; 0x3099; 0x0390];
+  ()
 
 let test_ccc () =
   assert (Uunf.ccc (Uchar.of_int 0x0020) = 0);

commit e93efa3f15920b0fdf3b8570f6ce1a7742448706

Fix regression introduced by 9459c90 (closes #10).

src/uunf.ml
@@ -189,17 +189,17 @@ let rec add n u =
 let compose n =                         (* canonical composition algorithm. *)
   let rec loop ~last_starter ~prev_ccc i =
     if i > n.last then () else
-    match _composite n.acc.(last_starter) n.acc.(i) with
-    | u when u = ux_none ->
-        let ccc_i = _ccc n.acc.(i) in
+    let ccc_i = _ccc n.acc.(i) in
+    let u_comp = _composite n.acc.(last_starter) n.acc.(i) in
+    match (u_comp = ux_none || (ccc_i = 0 && last_starter <> i - 1)) with
+    | true ->
         let last_starter = if ccc_i = 0 then i else last_starter in
         loop ~last_starter ~prev_ccc:ccc_i (i + 1)
-    | u ->
-        let ccc_i = _ccc n.acc.(i) in
+    | false ->
         match prev_ccc <> 0 && prev_ccc >= ccc_i with
         | true -> loop ~last_starter ~prev_ccc:ccc_i (i + 1)
         | false ->
-            n.acc.(last_starter) <- u;
+            n.acc.(last_starter) <- u_comp;
             Array.blit n.acc (i + 1) n.acc i (n.last - i);
             n.last <- n.last - 1;
             let prev_ccc = _ccc n.acc.(last_starter) in

test/test.ml
@@ -197,6 +197,8 @@ let test_others () =
   (* found by crowbar *)
   test [0x01C6; 0x032D] `NFKC [0x0064; 0x017E; 0x032D];
   test [0xFF80; 0x1FD3; 0xFF9E; 0x1FD3;] `NFKC [0x30BF; 0x0390; 0x3099; 0x0390];
+  (* found again by crowbar *)
+  test [0xC100; 0x20D2; 0x11C1; 0x11C1] `NFC [0xC100; 0x20D2; 0x11C1; 0x11C1];
   ()
 
 let test_ccc () =