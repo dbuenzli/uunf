f83999ccd6b87d1a1183d621e28aa2605b24274d
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-10-24 01:50:41 +0200

    Track b0.

--- B0.ml
+++ B0.ml
@@ -49,7 +49,7 @@ let test =
   let meta =
     B0_meta.(empty |> tag test |> tag run |> ~~ B0_unit.Action.cwd `Scope_dir)
   in
-  let requires = [ b0_std; uunf ] in
+  let requires = [b0_std; uunf; cmdliner] in
   B0_ocaml.exe "test_uunf" ~srcs ~meta ~requires ~doc:"Test normalization"
 
 let examples =
@@ -64,7 +64,7 @@ let uc_base = "http://www.unicode.org/Public"
 let show_version =
   B0_unit.of_action "unicode-version" ~doc:"Show supported unicode version" @@
   fun _ _ ~args:_ ->
-  Ok (Log.app (fun m -> m "%s" (String.of_version unicode_version)))
+  Ok (Log.stdout (fun m -> m "%s" (String.of_version unicode_version)))
 
 let download_tests =
   let doc = "Download the UCD normalization tests" in
@@ -72,7 +72,7 @@ let download_tests =
   let version = String.of_version unicode_version in
   let test_url = Fmt.str "%s/%s/ucd/NormalizationTest.txt" uc_base version in
   let test_file = B0_env.in_scope_dir env ~/"test/NormalizationTest.txt" in
-  (Log.app @@ fun m ->
+  (Log.stdout @@ fun m ->
    m "@[<v>Downloading %s@,to %a@]" test_url Fpath.pp test_file);
   B0_action_kit.fetch_url env test_url test_file
 
@@ -84,7 +84,7 @@ let download_ucdxml =
   let ucd_url = Fmt.str "%s/%s/ucdxml/ucd.all.grouped.zip" uc_base version in
   let ucd_file = B0_env.in_scope_dir env ~/"support/ucd.xml" in
   Result.join @@ Os.File.with_tmp_fd @@ fun tmpfile tmpfd ->
-  (Log.app @@ fun m ->
+  (Log.stdout @@ fun m ->
    m "@[<v>Downloading %s@,to %a@]" ucd_url Fpath.pp ucd_file);
   let* () = B0_action_kit.fetch_url env ucd_url tmpfile in
   let stdout = Os.Cmd.out_file ~force:true ~make_path:true ucd_file in
